57b11d343a5cb74e8945f9bc25056e7ac6caba41
ValenKono <147621279+ValenKono@users.noreply.github.com>
2025-08-12 10:09:56 -0300

    anda

---
+++ cuidridi.js
@@ -0,0 +1,24 @@
+const contactBtn = document.getElementById('contactBtn');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('closeModal');
+const registerBtn = document.getElementById('btnRegistrar');
+
+
+contactBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }   
+});
+
+registerBtn.addEventListener('click', () => {
+    window.open ('https://proyecto-production-c233.up.railway.app', "_blank");
+});
