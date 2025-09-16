413df12cb5b5e9643c43e360f9d498bd48babfcf
ValenKono <147621279+ValenKono@users.noreply.github.com>
2025-09-15 23:36:20 -0300

    Alonte Jardinero

--- cuidridi.js
+++ cuidridi.js
@@ -2,6 +2,7 @@ const contactBtn = document.getElementById('contactBtn');
 const modal = document.getElementById('modal');
 const closeModal = document.getElementById('closeModal');
 const registerBtn = document.getElementById('btnRegistrar');
+const closeRegistro = document.getElementById('closeRegistro');
 
 
 contactBtn.addEventListener('click', () => {
@@ -13,12 +14,46 @@ closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
+
+registerBtn.addEventListener('click', () => {
+    modalRegistro.style.display = 'flex';
+});
+
+// cerrar modal al tocar la X
+closeRegistro.addEventListener('click', () => {
+    modalRegistro.style.display = 'none';
+});
+
 window.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.style.display = 'none';
     }   
 });
 
-registerBtn.addEventListener('click', () => {
-    window.open ('https://proyecto-zvzl.onrender.com', "_blank");
+
+
+
+//registerBtn.addEventListener('click', () => {
+    //window.open ('https://proyecto-zvzl.onrender.com', "_blank");
+//});
+
+
+document.querySelectorAll(".faq-question").forEach(button => {
+    button.addEventListener("click", () => {
+        const answer = button.nextElementSibling;
+        const arrow = button.querySelector(".arrow");
+
+        // cerrar todas las demás
+        document.querySelectorAll(".faq-answer").forEach(ans => {
+            if (ans !== answer) ans.style.display = "none";
+        });
+        document.querySelectorAll(".arrow").forEach(arr => {
+            if (arr !== arrow) arr.classList.remove("rotate");
+        });
+
+        // alternar la seleccionada
+        const isOpen = answer.style.display === "block";
+        answer.style.display = isOpen ? "none" : "block";
+        arrow.classList.toggle("rotate", !isOpen);
+    });
 });
